a36bedac2bc1cc0b27d32b68477712afa29d25b1
Nguyen Hoang Duc Duy <nguyenhoangducduy1610@gmail.com>
2022-04-24 20:11:54 +0700

    add tour route & user route

--- super-tours/app.js
+++ super-tours/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
-
 const morgan = require("morgan");
+
+// My modules
+const tourRouter = require("./routes/tour-routes");
+const userRouter = require("./routes/user-routes");
 
 const app = express();
 
+// Logging
 app.use(morgan("dev"));
+
+// Body parser
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
